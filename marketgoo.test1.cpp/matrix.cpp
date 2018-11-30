bfe05c84e75401dfbc4aa493618702bc6c1c8f7d
aique <aiquefernandez@gmail.com>
2018-11-30 17:28:14 +0100

    matrix - init delay and refact done

--- marketgoo.test1.cpp/matrix.cpp
+++ marketgoo.test1.cpp/matrix.cpp
@@ -11,45 +11,37 @@
 using namespace std::this_thread;
 using namespace std::chrono;
 
-/**
- * Definimos un método "rgb" para un cálculo rápido del color a usar en un cell
- * con el método de colores de xterm-256color
- */
 #define TOCELL(x)  (x * 6 / 256)
 uint16_t rgb(int r, int g, int b)
 { return 16 + TOCELL(r) * 36 + TOCELL(g) * 6 + TOCELL(b); }
 
-class terminal_size
+class matrix_color
 {
     public:
 
-        int get_terminal_rows() {
-            return get_terminal_size().ws_row;
-        }
-
-        int get_terminal_columns() {
-            return get_terminal_size().ws_col / 2;
-        }
-
-    private:
-
-        winsize get_terminal_size() {
-            struct winsize size;
-            ioctl(STDOUT_FILENO,TIOCGWINSZ, &size);
-
-            return size;
-        }
-
+        const uint16_t WHITE = rgb(255, 255, 255);
+        const uint16_t WHITE_GREEN = rgb(150, 255, 150);
+        const uint16_t LIGHT_GREEN = rgb(0, 255, 0);
+        const uint16_t GREEN = rgb(0, 255, 0);
+        const uint16_t DARK_GREEN =  rgb(0, 100, 0);
+        const uint16_t INVISIBLE = rgb(0, 0, 0);
 };
 
 class matrix_cascade_values
 {
     public:
 
+        // número de caracteres ya visibles que se actualizarán aleatoriamente
+        int get_random_num_updatable_chars() {
+            return get_random_value_in_range(0, max_updatable_chars);
+        }
+
+        // número de caracteres que componen la cascada
         int get_random_num_visible_chars(int max_length) {
             return get_random_value_in_range(max_length / min_length_proportion, max_length);
         }
 
+        // velocidad de caída
         int get_random_delay() {
             return get_random_value_in_range(min_delay, max_delay);
         }
@@ -58,37 +50,20 @@ class matrix_cascade_values
             return get_random_value_in_range(first_unicode_char, last_unicode_char);
         }
 
+        // color del carácter en función de su posición en la cascada
         uint16_t get_character_color(unsigned int pos) {
             if (!has_assigned_color(pos)) {
-                return rgb(0, 100, 0);
+                return color.DARK_GREEN;
             }
 
             return char_colors[pos];
         }
 
-    private:
+        uint16_t get_invisible_color() {
+            return color.INVISIBLE;
+        }
 
-        uint32_t first_unicode_char = 0x30A0;
-        uint32_t last_unicode_char = 0x30FF;
-
-        int min_delay = 50;
-        int max_delay = 150;
-
-        int min_length_proportion = 4;
-
-        std::vector<uint16_t> char_colors = {
-            rgb(255, 255, 255),
-            rgb(150, 255, 150),
-            rgb(150, 255, 150),
-            rgb(0, 255, 0),
-            rgb(0, 255, 0),
-            rgb(0, 255, 0),
-            rgb(0, 175, 0),
-            rgb(0, 175, 0),
-            rgb(0, 175, 0),
-            rgb(0, 175, 0)
-        };
-
+        // devuelve un entero aleatorio dentro de un rango
         int get_random_value_in_range(int min, int max) {
             std::random_device rd;
             std::mt19937 rng(rd());
@@ -96,6 +71,38 @@ class matrix_cascade_values
 
             return uni(rng);
         }
+
+    private:
+
+        matrix_color color;
+
+        // número máximo de caracteres actualizados aleatoriamente
+        const int max_updatable_chars = 3;
+
+        // rango de caractéres que componen la cascada
+        const uint32_t first_unicode_char = 0x30A0;
+        const uint32_t last_unicode_char = 0x30FF;
+
+        // rango de valores para la velocidad de caída
+        const int min_delay = 50;
+        const int max_delay = 150;
+
+        // proporción mínima de la altura de la cascada con respecto a la terminal
+        const int min_length_proportion = 4;
+
+        // patrón de colores de la cascada en función de la posición del carácter
+        const std::vector<uint16_t> char_colors = {
+            color.WHITE,
+            color.WHITE_GREEN,
+            color.WHITE_GREEN,
+            color.LIGHT_GREEN,
+            color.LIGHT_GREEN,
+            color.LIGHT_GREEN,
+            color.GREEN,
+            color.GREEN,
+            color.GREEN,
+            color.GREEN
+        };
 
         bool has_assigned_color(unsigned int pos) {
             return pos < char_colors.size();
@@ -119,11 +126,17 @@ class matrix_cascade
 {
     public:
 
+        // posición de la cascada dentro de las columnas de la terminal
         int x;
+        // altura de la cascada, será del mismo tamaño que el alto de la terminal
         int length;
+        // velocidad de caída
         int delay;
+        // número de caracteres visibles dentro de la cascada
         int num_visible_chars;
+        // posición de la cabeza de los caracteres visibles de la cascada
         int visible_chars_head_position;
+        // conjunto de caractéres que componen la cascada
         std::vector<matrix_cascade_character> characters;
 
         matrix_cascade(int x, int length) {
@@ -135,15 +148,17 @@ class matrix_cascade
             characters = generate_characters(length);
         }
 
+        // hace avanzar un carácter a la cascada, actualiza aleatoriamente alguno de sus caracteres y adapta los colores
         void visible_chars_step_forward() {
             visible_chars_head_position++;
-            update_characters_color();
+            update_random_chars();
+            update_chars_color();
         }
 
-        void update_characters_color() {
+        void update_chars_color() {
             for (int i = visible_chars_head_position - num_visible_chars ; i <= visible_chars_head_position ; i++) {
                 if (valid_character_position(i)) {
-                    characters[i].color = calculate_character_color(i);
+                    characters[i].color = calculate_char_color(i);
                 }
             }
         }
@@ -152,17 +167,54 @@ class matrix_cascade
             return characters[pos];
         }
 
-        uint16_t calculate_character_color(int pos) {
+        uint16_t calculate_char_color(int pos) {
             if (is_invisible_char(pos)) {
-                return rgb(0, 0, 0);
+                return cascade_values.get_invisible_color();
             }
 
-            return cascade_values.get_character_color(abs(pos - visible_chars_head_position));
+            int position_in_visible_chars = abs(pos - visible_chars_head_position);
+            return cascade_values.get_character_color(position_in_visible_chars);
         }
 
     private:
 
         matrix_cascade_values cascade_values;
+
+        void update_random_chars() {
+            int num_updatable_chars = cascade_values.get_random_num_updatable_chars();
+
+            for (int i = 0 ; i < num_updatable_chars ; i++) {
+                int min_pos = get_updatable_chars_min_pos();
+                int max_pos = get_updatable_chars_max_pos();
+                int updatable_pos = cascade_values.get_random_value_in_range(min_pos, max_pos);
+
+                characters[updatable_pos].value = cascade_values.generate_random_char();
+            }
+        }
+
+        int get_updatable_chars_min_pos() {
+            int min_pos = visible_chars_head_position - num_visible_chars;
+
+            if (min_pos < 0) {
+                min_pos = 0;
+            }
+
+            if (min_pos > length - 1) {
+                min_pos = length - 1;
+            }
+
+            return min_pos;
+        }
+
+        int get_updatable_chars_max_pos() {
+            int max_pos = visible_chars_head_position;
+
+            if (max_pos > length - 1) {
+                max_pos = length - 1;
+            }
+
+            return max_pos;
+        }
 
         std::vector<matrix_cascade_character> generate_characters(int length) {
             std::vector<matrix_cascade_character> characters;
@@ -176,7 +228,7 @@ class matrix_cascade
 
         matrix_cascade_character generate_char(int pos) {
             uint32_t value = cascade_values.generate_random_char();
-            uint16_t color = calculate_character_color(pos);
+            uint16_t color = calculate_char_color(pos);
             matrix_cascade_character character(value, color);
 
             return character;
@@ -204,6 +256,49 @@ class matrix_console
             for (int i = 0 ; i < cascade.length ; i++) {
                 matrix_cascade_character character = cascade.get_char(i);
                 tb_change_cell(cascade.x, i, character.value, character.color, 0);
+            }
+        }
+};
+
+class matrix
+{
+    public:
+
+        int num_columns;
+        int num_rows;
+
+        matrix(int num_columns, int num_rows) {
+
+            this->num_columns = num_columns;
+            this->num_rows = num_rows;
+
+            for (int i = 0 ; i < num_columns ; i++) {
+                int init_delay = cascade_values.get_random_value_in_range(0, max_init_delay);
+                std::thread matrix_in_column(column, i * 2, num_rows, init_delay);
+                matrix_in_column.detach();
+            }
+        }
+
+    private:
+
+        matrix_cascade_values cascade_values;
+
+        // máximo valor para el delay inicial de cada cascada
+        const int max_init_delay = 5000;
+
+        static void column(int x, int num_rows, int init_delay) {
+            matrix_console console;
+
+            // permite que el efecto de la cascada se mantenga desde el comienzo
+            sleep_for(milliseconds(init_delay));
+
+            while (true) {
+                matrix_cascade cascade = matrix_cascade(x, num_rows);
+
+                while (cascade.visible_chars_head_position < num_rows + cascade.num_visible_chars) {
+                    console.update(cascade);
+                    cascade.visible_chars_step_forward();
+                }
             }
         }
 };
@@ -246,37 +341,25 @@ class termbox_handler
         }
 };
 
-class matrix
+class terminal_size
 {
     public:
 
-        int num_columns;
-        int num_rows;
+        int get_terminal_rows() {
+            return get_terminal_size().ws_row;
+        }
 
-        matrix(int num_columns, int num_rows) {
-
-            this->num_columns = num_columns;
-            this->num_rows = num_rows;
-
-            for (int i = 0 ; i < num_columns ; i++) {
-                std::thread matrix_in_column(enable_column, i * 2, num_rows);
-                matrix_in_column.detach();
-            }
+        int get_terminal_columns() {
+            return get_terminal_size().ws_col / 2;
         }
 
     private:
 
-        static void enable_column(int x, int num_rows) {
-            matrix_console console;
+        winsize get_terminal_size() {
+            struct winsize size;
+            ioctl(STDOUT_FILENO,TIOCGWINSZ, &size);
 
-            while (true) {
-                matrix_cascade cascade = matrix_cascade(x, num_rows);
-
-                while (cascade.visible_chars_head_position < num_rows + cascade.num_visible_chars) {
-                    console.update(cascade);
-                    cascade.visible_chars_step_forward();
-                }
-            }
+            return size;
         }
 };
 
